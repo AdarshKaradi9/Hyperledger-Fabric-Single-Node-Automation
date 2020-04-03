8cfe657d179be092af490f007cbe9e51a79de899
Adarsh Karadi <adarshkaradi009@gmail.com>
2020-04-03 14:31:37 +0530

    bootscript changed

--- bootscript.sh
+++ bootscript.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+cd /var/run
+sudo chmod 777 docker.sock
+cd -
+chmod 777 -R *
 ./init.sh
 echo "Enter the network details"
 python3 gen.py
-sudo chmod 777 *.*
+chmod 777 -R *
 echo "------------Launching the Network---------------"
 ./launch.sh
